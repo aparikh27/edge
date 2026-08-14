a84300b17cfb8f1a33df6df5cb6b9102dbf1fb88
Arav Parikh <aparikh27@g.ucla.edu>
2026-08-14 10:17:22 -0700

    Fixed placeholder implementation of message bus with threadsafequeue

--- messages/publisher.hpp
+++ messages/publisher.hpp
@@ -1,12 +1,18 @@
 #pragma once
+
 #include "coordinator.hpp"
 #include "message.hpp"
 
-class Publisher {
-    public:
-        Publisher(Coordinator* c);
-        void push(Message m);
-    private:
-        Coordinator* m_coordinator;
+namespace ember::messaging {
 
+class Publisher {
+public:
+    explicit Publisher(Coordinator* c);
+    
+    void push(const Message& m);
+
+private:
+    Coordinator* m_coordinator;
 };
+
+} // namespace ember::messaging
