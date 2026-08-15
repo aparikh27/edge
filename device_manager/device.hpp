3c55db697131fbaf4a018c584a57c3fdf81cc277
Arav Parikh <aparikh27@g.ucla.edu>
2026-08-15 15:38:32 -0700

    Shutdown a device

--- device_manager/device.hpp
+++ device_manager/device.hpp
@@ -24,12 +24,20 @@ class DeviceManager {
         bool initialize_device();
         bool start_device();
         bool stop_device();
-        bool shutdown_device(Device d);
+        bool shutdown_device(Device d) {
+            auto it = find(m_devices.begin(), m_devices.end(), d);
+            if (it == m_devices.end()) {
+                return false;
+            }
+            m_devices.erase(it);
+            return true;
+
+        }
 
     private:
         std::vector<Device> m_devices;
     
-}
+};
 
 
 }
