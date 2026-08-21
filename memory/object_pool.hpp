4a62bf037593618ec50d7a895764c466c68d0bf6
Arav Parikh <aparikh27@g.ucla.edu>
2026-08-21 09:14:22 -0700

    debugged memory class

--- memory/object_pool.hpp
+++ memory/object_pool.hpp
@@ -8,15 +8,24 @@ namespace ember::memory {
 template <typename T, size_t Capacity>
 class ObjectPool {
 public:
-    ObjectPool() : m_pool(sizeof(T), Capacity) {}
+    static_assert(Capacity > 0, "ObjectPool capacity must be non-zero");
+
+    ObjectPool() : m_pool(sizeof(T), Capacity, alignof(T)) {}
 
     template <typename... Args>
     T* acquire(Args&&... args) {
         void* mem = m_pool.allocate();
         if (!mem) return nullptr;
-        
-        // Placement new constructs T in the allocated memory chunk
-        return ::new (mem) T(std::forward<Args>(args)...);
+
+        // Placement new constructs T in the allocated memory chunk.
+        // If T's constructor throws, the chunk must go back to the pool
+        // rather than leaking it forever.
+        try {
+            return ::new (mem) T(std::forward<Args>(args)...);
+        } catch (...) {
+            m_pool.deallocate(mem);
+            throw;
+        }
     }
 
     void release(T* obj) {
